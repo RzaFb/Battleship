d911e83803c46899591c59596f2cc2ab0f4f4e7f
RzaFb <rzafrhb@gmail.com>
2021-02-09 19:19:34 +0330

    added ship to map function

--- main.c
+++ main.c
@@ -7,8 +7,9 @@
 #include <time.h>
 #include <math.h>
 
-int N = 10;
 char player1[10], player2[10];
+
+int Ship_lenght[10] = {5, 3, 3, 2, 2, 2, 1, 1, 1, 1};
 
 bool p1 = false;
 bool target = false;
@@ -30,6 +31,7 @@ typedef struct Pr
 
 typedef struct shp
 {
+    int explosion;
     int lenght;
     int x;
     int y;
@@ -38,13 +40,13 @@ typedef struct shp
     struct shp *next;
 }Ship;
 
-void gameBoard(Cell gameBoard[][N])
+void gameBoard(Cell gameBoard[][11])
 {
-    for(int i = 0; i < N; i++)
+    for(int i = 1; i < 11; i++)
     {
-        for(int j = 0; j < N; j++)
+        for(int j = 1; j < 11; j++)
         {
-            gameBoard[i][j].symbol = 'W';
+            gameBoard[i][j].symbol = 'O';
             gameBoard[i][j].x = i;
             gameBoard[i][j].y = j;
         }
@@ -68,17 +70,16 @@ void delay(unsigned int mseconds)
     while (goal > clock());
 }
 
-void showMap()
+void showMap(Cell gameBoard[][11])
 {
-    int position = 0;
-    printf("\t\t    A\t    B\t    C\t    D\t    E\t    F\t    G\t    H\t    I\t    J\n");
+    printf("\t\t    1\t    2\t    3\t    4\t    5\t    6\t    7\t    8\t    9\t    10\n");
     printf("\t\t---------------------------------------------------------------------------------\n\t");
-    for(int i = 0; i<N; i++)
+    for(int i = 1; i<11; i++)
     {
-        printf("     %d",i+1);
-        for(int j = 0; j<N ; j++)
+        printf("     %d",i);
+        for(int j = 1; j<11 ; j++)
         {
-            printf("\t|   O");
+            printf("\t|   %c", gameBoard[i][j].symbol);
         }
         printf("   |\n\t\t---------------------------------------------------------------------------------\n\t");
     }
@@ -147,33 +148,140 @@ void newProfile()
 }
 
 //function to add the ships to a list
-//input : head (aka pointer to first in the list)
-void pushShip(Ship** head_ref, int lenght)
+//input : head (aka pointer to first in the list) and gameboard of player
+
+void appendShip(Ship** head_ref,Cell gameBoard[][11], int ln)
 {
+    /* 1. allocate node */
     Ship* new_node = (Ship*) malloc(sizeof(Ship));
 
-    new_node->lenght  = lenght;
+    Ship *last = *head_ref;  /* used in step 5*/
 
-    printf("please enter coordination\n first row\t second collumn\t third direction\n example: 5 6 H\t input: ");
+    new_node->lenght  = Ship_lenght[ln];
 
-    int x,y,d;
+    showMap(gameBoard);
 
-    scanf("%d %d %c",&x,&y,&d);
+    printf("\n please enter coordination for ship with length of: %d\n\n first column (horizontal)\t second row(vertical)\t third direction\n\n example: 5 6 H\t input: ",new_node->lenght);
+
+    int x, y;
+    char d;
+
+    L1:scanf("%d %d %c",&y,&x,&d);
 
     system("cls");
+
+    if(x > 11 || x < 1 || y < 1 || y > 11)
+    {
+        showMap(gameBoard);
+        printf("\ninvalid input/s. please try again: ");
+        goto L1;
+    }
+    if(d != 'H')
+    {
+        if(d != 'V')
+        {
+            showMap(gameBoard);
+            printf("\ninvalid input/s. please try again with V or H: ");
+            goto L1;
+        }
+    }
+    //printf("bug?\t"); // debugger
+    if(d == 'V')
+    {
+        if(x + Ship_lenght[ln] > 10)
+        {
+            showMap(gameBoard);
+            printf("\n\tship crossing boder/s. please try again: ");
+            goto L1;
+        }
+        bool near = false; // to check if there are ships nearby
+        for (int i = x - 1; i < x + Ship_lenght[ln] + 1; i++)
+        {
+            for(int j = y - 1; j < y + 2; j++)
+            {
+                if(gameBoard[i][j].symbol == 'S')
+                    near = true;
+                //printf("bug?\t"); // debugger
+            }
+        }
+        if (near == true)
+        {
+            showMap(gameBoard);
+            printf("\n\tinput is too close to another ship\n try again please: ");
+            goto L1;
+        }
+        for(int i = 0; i < Ship_lenght[ln]; i++)
+            {
+                gameBoard[x + i][y].symbol = 'S';
+                //printf("bug? %d  ",i); // debugger
+            }
+        for (int i = x - 1; i < x + Ship_lenght[ln] + 1; i++)
+        {
+            for(int j = y - 1; j < y + 2; j++)
+            {
+                if(gameBoard[i][j].symbol != 'S')
+                    gameBoard[i][j].symbol = 'W';
+            }
+        }
+    }
+
+     if(d == 'H')
+    {
+        if(y + Ship_lenght[ln] > 10)
+        {
+            showMap(gameBoard);
+            printf("\n\tship crossing border/s. please try again: ");
+            goto L1;
+        }
+        bool near = false; // to check if there are ships nearby
+        for (int i = -1; i < 2; i++)
+        {
+            for(int j = -1; j < Ship_lenght[ln] + 2; j++)
+            {
+                if(gameBoard[x + i][y + j].symbol == 'S')
+                    near = true;
+                //printf("bug?\t"); // debugger
+            }
+        }
+        if (near == true)
+        {
+            showMap(gameBoard);
+            printf("\n\tinput is too close to another ship\n try again please: ");
+            goto L1;
+        }
+        for(int i = 0; i < Ship_lenght[ln]; i++)
+            {
+                gameBoard[x][y + i].symbol = 'S';
+                //printf("bug? %d  ",i); // debugger
+            }
+        for (int i = x - 1; i < x + 2; i++)
+        {
+            for(int j = y - 1; j < y + Ship_lenght[ln] + 1; j++)
+            {
+                if(gameBoard[i][j].symbol != 'S')
+                    gameBoard[i][j].symbol = 'W';
+            }
+        }
+    }
 
     new_node->direction = d;
     new_node->x = x;
     new_node->y = y;
 
-    new_node->next = (*head_ref);
 
-    (*head_ref)    = new_node;
-}
+    new_node->next = NULL;
 
-void setShips()
-{
-        showMap();
+    if (*head_ref == NULL)
+    {
+       *head_ref = new_node;
+       return;
+    }
+
+    while (last->next != NULL)
+        last = last->next;
+
+    last->next = new_node;
+    return;
 }
 
 int showMenu(int num)
@@ -211,21 +319,16 @@ int showMenu(int num)
     delay(2000);
     system ("cls");
 
-    //showMap();
-    //delay(5000);
-    system("cls");
-
     printf("1. Play with a friend\n\n" );   //coming soon
 
     printf("2. Play with bot\n\n");
 
     printf("3. Load game\n\n");             //coming soon
 
-    printf("4. Settings (unavailable)\n\n");//coming soon
-
-    printf("5. Quit game\n\n");
+    printf("4. Quit game\n\n");
 
     printf("Please enter your choice: ");
+
     scanf("%d",&num);
 
 
@@ -235,32 +338,57 @@ int showMenu(int num)
 int main()
 {
 
-    Ship *head = NULL;
-    pushShip(&head,5);
+    Ship *head1 = NULL;
+    Ship *head2 = NULL;
 
 
-    Cell p1_gameBoard[N][N],p2_gameBoard[N][N];
+    Cell p1_gameBoard[11][11],p2_gameBoard[11][11];
 
     gameBoard(p1_gameBoard);
+
     gameBoard(p2_gameBoard);
 
     int choice = showMenu(1);
-    if(choice == 5)
+
+    if(choice == 4)
+
     {
         showCredits();
+
         return 0;
     }else if(choice == 2)
     {
         system("cls");
+
         strcpy(player1,"BOT");
+
         p1 = true;
+
         profileList();
-        newProfile();
+
+        printf("choose profile or create new\nchoose: 1\ncreate: 2\n");
+
+        int cho;
+
+        scanf("%d",&cho);
+
+        if(cho == 1)
+        {
+            printf("enter your username: ");
+            char unm[10];
+            scanf("%s",unm);
+        }
+
+        if(cho == 2)
+            newProfile();
+
         system("cls");
-        getch();
-        system("cls");
-        setShips();
-        getch();
+
+        for(int i = 0; i < 10; i++)
+        {
+            appendShip(&head2, p2_gameBoard, i);
+            //printf(" bug \t");  //manual debug
+        }
     }
 
     showCredits();
